e8c08bf9c42540da9ab7f91601c0dd7935ab1c9a
Joe Kariuki <jknguru6@gmail.com>
2024-01-02 12:29:43 +0300

    Add navbar and footer component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Camping Travel",
@@ -13,7 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Navbar />
+      <main className="relative overflow-hidden">{children}</main>
+      <Footer />
     </html>
   );
 }
